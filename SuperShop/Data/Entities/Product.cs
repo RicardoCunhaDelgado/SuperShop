152f63633b37b8d77d67c8ad7d12cb63fe1a7a79
Ricardo Delgado <ricardofoxbs@gmail.com>
2021-12-20 23:12:50 +0000

    Generic Repository

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SuperShop.Data.Entities
 {
-    public class Product
+    public class Product : IEntity
     {
-        
+
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(50, ErrorMessage ="The field {0} can contain {1} characters length.")]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
