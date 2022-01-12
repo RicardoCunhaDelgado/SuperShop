d55173446e64c2f808506652bf3fc19fd63b3565
Ricardo Delgado <ricardofoxbs@gmail.com>
2022-01-12 19:27:03 +0000

    Login and Logout

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SuperShop.Data;
 using SuperShop.Data.Entities;
@@ -11,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace SuperShop.Controllers
 {
+    [Authorize]
     public class ProductsController : Controller
     {
         private readonly IProductRepository _productRepository;
